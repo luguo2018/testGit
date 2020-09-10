bed05000e5866275c3284a8e2cd1b800eba86133
xuqiang <XQ6215**>
2020-09-10 15:09:55 +0800

    权限申请页适配UI、修改文案

--- app/src/main/java/com/jmhy/sdk/activity/FloatUserInfoActivity.java
+++ app/src/main/java/com/jmhy/sdk/activity/FloatUserInfoActivity.java
@@ -2,18 +2,16 @@ package com.jmhy.sdk.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
-import android.util.DisplayMetrics;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -27,7 +25,6 @@ import com.jmhy.sdk.model.BaseFloatActivity;
 import com.jmhy.sdk.utils.AndroidBug5497Workaround;
 import com.jmhy.sdk.utils.MimeType;
 import com.jmhy.sdk.utils.checkEmulator.FloatJsInterface;
-import com.jmhy.sdk.view.FloatView;
 import com.jmhy.sdk.view.GifImageView;
 
 import java.util.ArrayList;
@@ -36,53 +33,61 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
     private static final String TAG = "FloatUserInfoActivity";
     private WebView mWebview;
     private GifImageView mGifImageView;
-    private View parent,back_view;
+    private View parent;
     private String mUrl;
     private ValueCallback<Uri[]> uploadMessageAboveL;
     private ValueCallback<Uri> uploadMessage;
     private final static int FILE_CHOOSER_RESULT_CODE = 0x01;
-    private float originalY;
-    private boolean notice;
-    private boolean protocol;
+    private int mWbeHeight;
+    public boolean notice;
+    public boolean protocol;
     private View right_back;
-    private CloseFloatListener closeFloatListener;
-
-    public FloatUserInfoActivity(Activity activity,CloseFloatListener listener) {
+    private boolean isShowKeyboard;
+    private int reduceHeight;
+    public FloatUserInfoActivity(Activity activity) {
         super(activity);
-        this.closeFloatListener=listener;
+    }
+
+    @Override
+    public void setContentView(@NonNull int layout_id) {
+        super.setContentView(layout_id);
     }
 
     @Override
     public void setViews(String url) {
+        if(contentView!=null){
+            return;
+        }
         mUrl = url;
         intView();
-        originalY = mWebview.getY();
-        //防止软键盘覆盖webview
-        DisplayMetrics outMetrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
-        int widthPixels = outMetrics.widthPixels;
-        final int heightPixels = outMetrics.heightPixels;
 
+        //防止软键盘覆盖webview
         mWebview.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                Rect rect = new Rect();
-                Log.d(TAG, "onGlobalLayout: 软键盘状态改变"+heightPixels);
-                mWebview.getWindowVisibleDisplayFrame(rect);
-                int height = mWebview.getHeight() - rect.bottom;
-                ViewGroup.LayoutParams layoutParams = mWebview.getLayoutParams();
-                if (height > 100) {
-                    if (mWebview.getY() >= 0) {
-                        int[] location = new int[2];
-                        mWebview.getLocationInWindow(location);
-                        int h = location[1] + mWebview.getHeight() - rect.bottom;
-                        layoutParams.height = rect.bottom - rect.top;
+                Log.d(TAG, "onGlobalLayout: 软键盘状态改变");
+                if(mWebview!=null){
+                    mWbeHeight = mWebview.getHeight();
+                    Rect r = new Rect();
+                    //获取当前界面可视部分
+                    activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+                    //获取屏幕的高度
+                    int screenHeight =  activity.getWindow().getDecorView().getRootView().getHeight();
+                    //此处就是用来获取键盘的高度的， 在键盘没有弹出的时候 此高度为0 键盘弹出的时候为一个正数
+                    int heightDifference = screenHeight - r.bottom;
+                    Log.d("Keyboard Size", "Size: " + heightDifference);
+                    FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) mWebview.getLayoutParams();
+                    if (heightDifference>150&&!isShowKeyboard){
+                        reduceHeight =mWbeHeight-(mWbeHeight-heightDifference) ;
+                        layoutParams.height = mWbeHeight-heightDifference;
+                        mWebview.setLayoutParams(layoutParams);
+                        isShowKeyboard = true;
+                    }else if (heightDifference<150&&isShowKeyboard){
+                        isShowKeyboard =false;
+                        layoutParams.height = mWbeHeight+reduceHeight;
+                        mWebview.setLayoutParams(layoutParams);
                     }
-
-                } else {
-                    layoutParams.height = heightPixels;
                 }
-                mWebview.setLayoutParams(layoutParams);
             }
         });
     }
@@ -101,6 +106,12 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
                     viewGroup1.removeAllViews();
                     Log.d(TAG, "removeContentView: childCount=" + count);
                     viewGroup.removeView(contentView);
+                    mWebview=null;
+                    mGifImageView=null;
+                    FloatUserInfoActivity.this.parent=null;
+                    uploadMessage=null;
+                    uploadMessageAboveL=null;
+                    contentView=null;
                 }
             });
 
@@ -134,33 +145,19 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
         }
 
         setContentView(layout_id);
-//        if (getContentView().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
         if (true) {
-//            AndroidBug5497Workaround.assistActivity(activity);
+            AndroidBug5497Workaround.assistActivity(activity);
         }
         // TODO Auto-generated method stub
         parent = contentView.findViewById((AppConfig.resourceId(activity, "parent", "id")));
         right_back =contentView.findViewById((AppConfig.resourceId(activity, "right_back", "id")));
-        back_view =contentView.findViewById((AppConfig.resourceId(activity, "back_view", "id")));
-        if (parent != null) {
-            parent.setOnClickListener(this);
-        }
-        if (right_back != null) {
-            right_back.setOnClickListener(this);
-        }
-        if (back_view != null) {
-            back_view.setOnClickListener(this);
-        }
-        
+
+
         mWebview = (WebView) contentView.findViewById(AppConfig.resourceId(activity,
                 "webview", "id"));
         mGifImageView = (GifImageView) contentView.findViewById(AppConfig.resourceId(activity,
                 "gif", "id"));
         switch (AppConfig.skin) {
-            case 9:
-                mGifImageView.setGifResource(AppConfig.resourceId(activity, "jmloading_9",
-                        "drawable"));
-                break;
             case 8:
                 mGifImageView.setGifResource(AppConfig.resourceId(activity, "jmloading_new",
                         "drawable"));
@@ -173,6 +170,7 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
                 mGifImageView.setGifResource(AppConfig.resourceId(activity, "jmloading_new",
                         "drawable"));
                 break;
+            case 5:
             case 4:
                 mGifImageView.setGifResource(AppConfig.resourceId(activity, "jmloading_4",
                         "drawable"));
@@ -197,33 +195,8 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
             Log.e("JiMiSDK", "JmuserinfoActivity view == null");
             return;
         }
-        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view.getLayoutParams();
         switch (AppConfig.skin) {
-            case 9:
-                DisplayMetrics outMetrics = new DisplayMetrics();
-                activity.getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
-                int widthPixels = outMetrics.widthPixels;
-                int heightPixels = outMetrics.heightPixels;
-                if (notice){
-                }else if (protocol){
-                    FrameLayout.LayoutParams linearParams2 = (FrameLayout.LayoutParams) mWebview.getLayoutParams();
-                    linearParams2.width = widthPixels;
-                    linearParams2.height = heightPixels;
-                    mWebview.setLayoutParams(linearParams2);
-                }
-                else{
-                    if (getContentView().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {//竖屏
-                        FrameLayout.LayoutParams linearParams2 = (FrameLayout.LayoutParams) mWebview.getLayoutParams();
-                        linearParams2.width = (int) (widthPixels * 0.8);
-//                        linearParams2.height = (int) (heightPixels*0.6);
-                        mWebview.setLayoutParams(linearParams2);
-                    } else {
-                        FrameLayout.LayoutParams linearParams2 = (FrameLayout.LayoutParams) mWebview.getLayoutParams();
-                        linearParams2.width = (int) (heightPixels * 1.2);
-                        mWebview.setLayoutParams(linearParams2);
-                    }
-                }
-                break;
+            case 5:
             case 4:
                 //layoutParams.width = (int)getResources().getDimension(AppConfig.resourceId(activity, "jm_login_width_4", "dimen"));
                 //layoutParams.height = (int)getResources().getDimension(AppConfig.resourceId(activity, "jm_login_height_4", "dimen"));
@@ -241,7 +214,7 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
         mWebview.addJavascriptInterface(new FloatJsInterface(activity, this), "jimiJS");
         mWebview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         // 修复一些机型webview无法点击****/
-//        mWebview.requestFocus(View.FOCUS_DOWN);
+        mWebview.requestFocus(View.FOCUS_DOWN);
         mWebview.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -276,6 +249,8 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
             }
         });
 
+        parent.setOnClickListener(this);
+        right_back.setOnClickListener(this);
         mWebview.setWebChromeClient(new WebChromeClient() {
             public void openFileChooser(ValueCallback<Uri> valueCallback) {
                 uploadMessage = valueCallback;
@@ -295,22 +270,20 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
 
                 //h5文件选择回调，监听类型传对应格式打开文件选择器
                 ArrayList fileType = new ArrayList();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    for (int i = 0; i < fileChooserParams.getAcceptTypes().length; i++) {
-                        Log.i(TAG, "查看webview打开文件选择器的文件类型：" + fileChooserParams.getAcceptTypes()[i]);
-                        if (fileChooserParams.getAcceptTypes()[i].equals("image/png")) {
-                            fileType.add(MimeType.IMAGE);
-                        } else if (fileChooserParams.getAcceptTypes()[i].equals("video/mp4")) {
-                            fileType.add(MimeType.VEDIO);
-                        } else if (fileChooserParams.getAcceptTypes()[i].equals("application/msword")) {
-                            fileType.add(MimeType.DOC);
-                            fileType.add(MimeType.DOCX);
-                            fileType.add(MimeType.PDF);
-                            fileType.add(MimeType.PPT);
-                            fileType.add(MimeType.PPTX);
-                            fileType.add(MimeType.XLS);
-                            fileType.add(MimeType.XLSX);
-                        }
+                for (int i = 0; i < fileChooserParams.getAcceptTypes().length; i++) {
+                    Log.i(TAG, "查看webview打开文件选择器的文件类型：" + fileChooserParams.getAcceptTypes()[i]);
+                    if (fileChooserParams.getAcceptTypes()[i].equals("image/png")) {
+                        fileType.add(MimeType.IMAGE);
+                    } else if (fileChooserParams.getAcceptTypes()[i].equals("video/mp4")) {
+                        fileType.add(MimeType.VEDIO);
+                    } else if (fileChooserParams.getAcceptTypes()[i].equals("application/msword")) {
+                        fileType.add(MimeType.DOC);
+                        fileType.add(MimeType.DOCX);
+                        fileType.add(MimeType.PDF);
+                        fileType.add(MimeType.PPT);
+                        fileType.add(MimeType.PPTX);
+                        fileType.add(MimeType.XLS);
+                        fileType.add(MimeType.XLSX);
                     }
                 }
                 Log.i("测试webV文件打开格式", "类型" + fileType);
@@ -357,16 +330,8 @@ public class FloatUserInfoActivity extends BaseFloatActivity {
             }
         } else if (v.getId() ==AppConfig.resourceId(activity, "right_back", "id")) {
             removeContentView();
-            closeFloatListener.closeFloat();
-        } else if (v.getId() ==AppConfig.resourceId(activity, "back_view", "id") ) {
-            removeContentView();
-            closeFloatListener.closeFloat();
+        } else if (v.getId() ==AppConfig.resourceId(activity, "parent", "id") ) {
+
         }
     }
-    public interface CloseFloatListener {
-
-        void closeFloat();
-
-    }
 }
-
