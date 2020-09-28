d6b3f98657005ec2cd3d48086d6624578a03e0bc
luowei@jmhy.com <whndswhbs520>
2020-09-28 19:59:40 +0800

    弹窗修改

--- app/src/main/java/com/jmhy/sdk/activity/JmBaseActivity.java
+++ app/src/main/java/com/jmhy/sdk/activity/JmBaseActivity.java
@@ -29,191 +29,196 @@ import com.jmhy.sdk.utils.Utils;
 
 public class JmBaseActivity extends Activity {
 
-	public String appKey = "", serverId = "", verId = "";
-	public int appId;
-	public Seference mSeference;
-	public UserInfo mUserinfo;
+    public String appKey = "", serverId = "", verId = "";
+    public int appId;
+    public Seference mSeference;
+    public UserInfo mUserinfo;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);// 隐藏标题
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
-		Utils.getSeferencegame(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);// 隐藏标题
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
+        Utils.getSeferencegame(this);
 
-		mSeference = new Seference(this);
-		mUserinfo = new UserInfo();
-	}
+        mSeference = new Seference(this);
+        mUserinfo = new UserInfo();
+    }
 
-	private void initData() {
-	
-	}
-	/**
-	 * 接口返回数据处理
-	 */
-	public void sendData(int num, Object data, Handler callback) {
-		Message msg = callback.obtainMessage();
-		msg.what = num;
-		msg.obj = data;
-		msg.sendToTarget();
-	}
+    private void initData() {
 
-	/**
-	 * toast 提示信息
-	 * 
-	 * @param msg
-	 */
-	public void showMsg(String msg) {
-		DialogUtils.showTip(this, msg);
-	}
-	public void showUserMsg(String msg) {
-		String s = "";
-		if (msg.length() > 8) {
-			s = msg.substring(0, 8) + "...";
-		}else{
-			s=msg;
-		}
-		Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
-		View layout = View.inflate(this,
-				AppConfig.resourceId(this, "jmtoast", "layout"), null);
-		TextView user = (TextView) layout.findViewById(AppConfig.resourceId(this, "tvuser", "id"));
-		// 设置toast文本，把设置好的布局传进来
-		user.setText(s+"");
-		toast.setView(layout);
-		toast.setGravity(Gravity.TOP, 0, 50);
-		toast.show();
-	}
-	/**
-	 * Intent页面跳转
-	 */
-	public void changePage(Activity activity, Class<?> cls) {
-		Intent intent = new Intent(activity, cls);
-		Bundle bundle = new Bundle();
-		intent.putExtras(bundle);
-		activity.startActivity(intent);
-	}
+    }
 
-	/**
-	 * 显示登录成功
-	 * 
-	 * @param userName
-	 * @param xOffset
-	 * @param yOffset
-	 * @param duration
-	 */
-	public void showLoginFinish(String userName, int xOffset, int yOffset,
-			int duration) {
-	}
+    /**
+     * 接口返回数据处理
+     */
+    public void sendData(int num, Object data, Handler callback) {
+        Message msg = callback.obtainMessage();
+        msg.what = num;
+        msg.obj = data;
+        msg.sendToTarget();
+    }
 
-	/**
-	 * 回调信息
-	 */
-	public void callBack(String result, String msg, String timestamp,
-			String uid, String username, String sign,String token) {
-	}
+    /**
+     * toast 提示信息
+     *
+     * @param msg
+     */
+    public void showMsg(String msg) {
+        DialogUtils.showTip(this, msg);
+    }
+
+    public void showUserMsg(String msg) {
+        String s = "";
+        if (msg.length() > 8) {
+            s = msg.substring(0, 8) + "...";
+        } else {
+            s = msg;
+        }
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        View layout = View.inflate(this,
+                AppConfig.resourceId(this, "jmtoast", "layout"), null);
+        TextView user = (TextView) layout.findViewById(AppConfig.resourceId(this, "tvuser", "id"));
+        // 设置toast文本，把设置好的布局传进来
+        user.setText(s + "");
+        toast.setView(layout);
+        toast.setGravity(Gravity.TOP, 0, 50);
+        toast.show();
+    }
+
+    /**
+     * Intent页面跳转
+     */
+    public void changePage(Activity activity, Class<?> cls) {
+        Intent intent = new Intent(activity, cls);
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        activity.startActivity(intent);
+    }
+
+    /**
+     * 显示登录成功
+     *
+     * @param userName
+     * @param xOffset
+     * @param yOffset
+     * @param duration
+     */
+    public void showLoginFinish(String userName, int xOffset, int yOffset,
+                                int duration) {
+    }
+
+    /**
+     * 回调信息
+     */
+    public void callBack(String result, String msg, String timestamp,
+                         String uid, String username, String sign, String token) {
+    }
 
 
-	/**
-	 * 判断输入框中的数据是否符合格式
-	 */
-	public boolean verfy(EditText user, EditText pwd) {
+    /**
+     * 判断输入框中的数据是否符合格式
+     */
+    public boolean verfy(EditText user, EditText pwd) {
 
-		if (user != null && pwd != null) {
-			if (user.getText() == null || "".equals(user.getText().toString())) {
-				String emptyAccount = AppConfig.getString(this, "user_hintuser_msg");
-				showMsg(emptyAccount);
-				return true;
-			} else if (pwd.getText() == null
-					|| "".equals(pwd.getText().toString())) {
-				String emptyPassword = AppConfig.getString(this, "user_hintpwd_msg");
-				showMsg(emptyPassword);
-				return true;
-			}
-		}
-		if (user == null) {
-			if (pwd.getText() == null || "".equals(pwd.getText().toString())) {
-				String emptyPassword = AppConfig.getString(this, "user_hintpwd_msg");
-				showMsg(emptyPassword);
-				return true;
-			}
-		}
+        if (user != null && pwd != null) {
+            if (user.getText() == null || "".equals(user.getText().toString())) {
+                String emptyAccount = AppConfig.getString(this, "user_hintuser_msg");
+                showMsg(emptyAccount);
+                return true;
+            } else if (pwd.getText() == null
+                    || "".equals(pwd.getText().toString())) {
+                String emptyPassword = AppConfig.getString(this, "user_hintpwd_msg");
+                showMsg(emptyPassword);
+                return true;
+            }
+        }
+        if (user == null) {
+            if (pwd.getText() == null || "".equals(pwd.getText().toString())) {
+                String emptyPassword = AppConfig.getString(this, "user_hintpwd_msg");
+                showMsg(emptyPassword);
+                return true;
+            }
+        }
 
-		if (pwd == null) {
-			if (user.getText() == null || "".equals(user.getText().toString())) {
-				String emptyAccount = AppConfig.getString(this, "user_hintuser_msg");
-				showMsg(emptyAccount);
-				return true;
-			}
-		}
-		return false;
-	}
+        if (pwd == null) {
+            if (user.getText() == null || "".equals(user.getText().toString())) {
+                String emptyAccount = AppConfig.getString(this, "user_hintuser_msg");
+                showMsg(emptyAccount);
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public void addFragmentToActivity(FragmentManager fragmentManager,
-			Fragment fragment, int frameId) {
-		Utils.checkNotNull(fragmentManager);
-		Utils.checkNotNull(fragment);
-		FragmentTransaction transaction = fragmentManager.beginTransaction();
-		transaction.add(frameId, fragment);
-		transaction.commit();
-	}
+    public void addFragmentToActivity(FragmentManager fragmentManager,
+                                      Fragment fragment, int frameId) {
+        Utils.checkNotNull(fragmentManager);
+        Utils.checkNotNull(fragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(frameId, fragment);
+        transaction.commit();
+    }
 
-	public void wrapaLoginInfo(String result, String msg, String userName,
-			String openid, String gametoken) {
-		Log.i("JiMiSDK", "wrapaLoginInfo result=" + result+",msg=" +msg + ",openid=" + openid + ",gametoken=" + gametoken+",userName="+userName);
+    public void wrapaLoginInfo(String result, String msg, String userName,
+                               String openid, String gametoken) {
+        Log.i("JiMiSDK", "wrapaLoginInfo result=" + result + ",msg=" + msg + ",openid=" + openid + ",gametoken=" + gametoken + ",userName=" + userName);
 
-		if(TextUtils.isEmpty(openid) || TextUtils.isEmpty(gametoken)){
-			Log.w("JiMiSDK", "wrapaLoginInfo openid or gametoken is null");
-			return;
-		}
-		if(result.equals("success")){
-			AppConfig.isShow = true;
-		}
+        if (TextUtils.isEmpty(openid) || TextUtils.isEmpty(gametoken)) {
+            Log.w("JiMiSDK", "wrapaLoginInfo openid or gametoken is null");
+            return;
+        }
+        if (result.equals("success")) {
+            AppConfig.isShow = true;
+        }
 
-		JiMiSDK.getStatisticsSDK().onLogin(openid);
+        JiMiSDK.getStatisticsSDK().onLogin(openid);
 
-		AppConfig.openid=openid;
-		LoginMessageinfo login = new LoginMessageinfo();
-		login.setMsg(msg);
-		login.setResult(result);
-		login.setGametoken(gametoken);
-		login.setUname(openid);
-		login.setOpenid(openid);
-		Message mssg = new Message();
-		mssg.obj = login;
-		mssg.what = 1;
-		JiMiSDK.handler.sendMessage(mssg);
-	}
-	public void toUsetlogin(Registermsg registermsg){
-		Intent intent = new Intent(this, JmLoginActivity.class);
-		this.startActivity(intent);
-		}
-	public void turnToIntent(String url) {
-		if (TextUtils.isEmpty(url)) {
-			// Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
-			return;
-		}
-	
-		  Intent intent = new Intent();
-		  intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-		  	Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		  intent.putExtra("url", url);
-		  intent.setClass(this, JmUserinfoActivity.class);
-		  startActivity(intent);
-		
-	}
+        AppConfig.openid = openid;
+        LoginMessageinfo login = new LoginMessageinfo();
+        login.setMsg(msg);
+        login.setResult(result);
+        login.setGametoken(gametoken);
+        login.setUname(openid);
+        login.setOpenid(openid);
+        Message mssg = new Message();
+        mssg.obj = login;
+        mssg.what = 1;
+        JiMiSDK.handler.sendMessage(mssg);
+    }
 
-	public void turnToNotice(final String url) {
-		if (TextUtils.isEmpty(url)) {
-			// Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
-			return;
-		}
+    public void toUsetlogin(Registermsg registermsg) {
+        Intent intent = new Intent(this, JmLoginActivity.class);
+        this.startActivity(intent);
+    }
 
-		JiMiSDK.handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				Log.i("jimisdk", "turnToNotice-------------------------");
+    public void turnToIntent(String url) {
+        if (TextUtils.isEmpty(url)) {
+            // Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra("url", url);
+        intent.setClass(this, JmUserinfoActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void turnToNotice(final String url) {
+        if (TextUtils.isEmpty(url)) {
+            // Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        JiMiSDK.handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("jimisdk", "turnToNotice-------------------------");
 
 //				Intent intent = new Intent();
 //				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
@@ -222,36 +227,30 @@ public class JmBaseActivity extends Activity {
 //				intent.putExtra("notice", true);
 //				intent.setClass(JiMiSDK.mContext, JmUserinfoActivity.class);
 //				JiMiSDK.mContext.startActivity(intent);
-				FloatUserInfoActivity floatUserInfoActivity = null;
-				if(floatUserInfoActivity==null){
-					floatUserInfoActivity = new FloatUserInfoActivity((Activity) JiMiSDK.mContext, new FloatUserInfoActivity.CloseFloatListener() {
-						@Override
-						public void closeFloat() {
-						}
-					});
-					floatUserInfoActivity.notice=true;
-					floatUserInfoActivity.setViews(url);
-					floatUserInfoActivity.show();
-				}else {
-					floatUserInfoActivity.setViews(url);
-					floatUserInfoActivity.show();
-				}
-			}
-		}, 1000);
-	}
+				FloatUserInfoActivity floatUserInfoActivity = new FloatUserInfoActivity((Activity) JiMiSDK.mContext, new FloatUserInfoActivity.CloseFloatListener() {
+                    @Override
+                    public void closeFloat() {
+                    }
+                });
+                floatUserInfoActivity.notice = true;
+                floatUserInfoActivity.setViews(url);
+                floatUserInfoActivity.show();
+            }
+        }, 1000);
+    }
 
-	public void turnToSetAccount() {
-		JiMiSDK.handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				Log.i("jimisdk", "turnToSetAccount-------------------------");
+    public void turnToSetAccount() {
+        JiMiSDK.handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("jimisdk", "turnToSetAccount-------------------------");
 
-				Intent intent = new Intent();
-				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-						Intent.FLAG_ACTIVITY_SINGLE_TOP);
-				intent.setClass(JiMiSDK.mContext, JmSetAccountActivity9.class);
-				JiMiSDK.mContext.startActivity(intent);
-			}
-		}, 1000);
-	}
+                Intent intent = new Intent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.setClass(JiMiSDK.mContext, JmSetAccountActivity9.class);
+                JiMiSDK.mContext.startActivity(intent);
+            }
+        }, 1000);
+    }
 }
--- app/src/main/java/com/jmhy/sdk/fragment/JmBaseFragment.java
+++ app/src/main/java/com/jmhy/sdk/fragment/JmBaseFragment.java
@@ -1,5 +1,6 @@
 package com.jmhy.sdk.fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jmhy.sdk.activity.FloatUserInfoActivity;
 import com.jmhy.sdk.activity.JmAutoLoginActivity;
 import com.jmhy.sdk.activity.JmUserinfoActivity;
 import com.jmhy.sdk.common.JiMiSDK;
@@ -29,209 +31,213 @@ import com.jmhy.sdk.utils.UserInfo;
 import com.jmhy.sdk.utils.Utils;
 
 public class JmBaseFragment extends Fragment implements View.OnTouchListener {
-	public Seference mSeference;
-	public UserInfo mUserinfo;
+    public Seference mSeference;
+    public UserInfo mUserinfo;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		mSeference = new Seference(getActivity());
-		mUserinfo = new UserInfo();
-		Utils.getSeferencegame(getActivity());
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        mSeference = new Seference(getActivity());
+        mUserinfo = new UserInfo();
+        Utils.getSeferencegame(getActivity());
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		return super.onCreateView(inflater, container, savedInstanceState);
-	}
-	FragmentTransaction transaction;
-	public void addFragmentToActivity(FragmentManager fragmentManager,
-			Fragment fragment, int frameId) {
-		Utils.checkNotNull(fragmentManager);
-		Utils.checkNotNull(fragment);
-		transaction = fragmentManager.beginTransaction();
-		transaction.add(frameId, fragment).addToBackStack(null);
-		transaction.commit();
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
-	public void replaceFragmentToActivity(FragmentManager fragmentManager,
-			Fragment fragment, int frameId) {
-		Utils.checkNotNull(fragmentManager);
-		Utils.checkNotNull(fragment);
-		transaction = fragmentManager.beginTransaction();
-		transaction.replace(frameId, fragment).addToBackStack(null);
-		transaction.commit();
-	}
+    FragmentTransaction transaction;
 
-	public void callKefu() {
-		Intent intent = new Intent();
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		intent.putExtra("url", AppConfig.KEFU);
-		intent.putExtra("notice", true);
-		intent.setClass(getActivity(), JmUserinfoActivity.class);
+    public void addFragmentToActivity(FragmentManager fragmentManager,
+                                      Fragment fragment, int frameId) {
+        Utils.checkNotNull(fragmentManager);
+        Utils.checkNotNull(fragment);
+        transaction = fragmentManager.beginTransaction();
+        transaction.add(frameId, fragment).addToBackStack(null);
+        transaction.commit();
+    }
 
-		startActivity(intent);
-	}
-	public void removeFragmentToActivity(FragmentManager fragmentManager,Fragment fragment) {
-		Utils.checkNotNull(fragmentManager);
-		Utils.checkNotNull(fragment);
-		transaction = fragmentManager.beginTransaction();
-		transaction.remove(fragment);
-		transaction.commit();
-	}
+    public void replaceFragmentToActivity(FragmentManager fragmentManager,
+                                          Fragment fragment, int frameId) {
+        Utils.checkNotNull(fragmentManager);
+        Utils.checkNotNull(fragment);
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(frameId, fragment).addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void callKefu() {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra("url", AppConfig.KEFU);
+        intent.putExtra("notice", true);
+        intent.setClass(getActivity(), JmUserinfoActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void removeFragmentToActivity(FragmentManager fragmentManager, Fragment fragment) {
+        Utils.checkNotNull(fragmentManager);
+        Utils.checkNotNull(fragment);
+        transaction = fragmentManager.beginTransaction();
+        transaction.remove(fragment);
+        transaction.commit();
+    }
 
 
+    public void wrapaLoginInfo(String result, String msg, String userName,
+                               String openid, String gametoken) {
+        Log.i("JiMiSDK", "wrapaLoginInfo result=" + result + ",msg=" + msg + ",openid=" + openid + ",gametoken=" + gametoken + ",userName=" + userName);
 
+        if (TextUtils.isEmpty(openid) || TextUtils.isEmpty(gametoken)) {
+            Log.w("JiMiSDK", "wrapaLoginInfo openid or gametoken is null");
+            return;
+        }
 
-	public void wrapaLoginInfo(String result, String msg, String userName,
-			String openid, String gametoken) {
-		Log.i("JiMiSDK", "wrapaLoginInfo result=" + result+",msg=" +msg + ",openid=" + openid + ",gametoken=" + gametoken+",userName="+userName);
+        if (result.equals("success")) {
+            AppConfig.isShow = true;
+        }
 
-		if(TextUtils.isEmpty(openid) || TextUtils.isEmpty(gametoken)){
-			Log.w("JiMiSDK", "wrapaLoginInfo openid or gametoken is null");
-			return;
-		}
+        JiMiSDK.getStatisticsSDK().onLogin(openid);
 
-		if(result.equals("success")){
-			AppConfig.isShow = true;
-		}
+        LoginMessageinfo login = new LoginMessageinfo();
+        AppConfig.openid = openid;
+        login.setMsg(msg);
+        login.setResult(result);
+        login.setGametoken(gametoken);
+        login.setUname(openid);
+        login.setOpenid(openid);
+        Message mssg = new Message();
+        mssg.obj = login;
+        mssg.what = 1;
+        JiMiSDK.handler.sendMessage(mssg);
+    }
 
-		JiMiSDK.getStatisticsSDK().onLogin(openid);
+    /**
+     * 接口返回数据处理
+     */
+    public void sendData(int num, Object data, Handler callback) {
+        Message msg = callback.obtainMessage();
+        msg.what = num;
+        msg.obj = data;
+        msg.sendToTarget();
+    }
 
-		LoginMessageinfo login = new LoginMessageinfo();
-		AppConfig.openid=openid;
-		login.setMsg(msg);
-		login.setResult(result);
-		login.setGametoken(gametoken);
-		login.setUname(openid);
-		login.setOpenid(openid);
-		Message mssg = new Message();
-		mssg.obj = login;
-		mssg.what = 1;
-		JiMiSDK.handler.sendMessage(mssg);
-	}
+    /**
+     * toast 提示信息
+     *
+     * @param msg
+     */
+    public void showUserMsg(String msg) {
+        String s = "";
+        if (msg.length() > 8) {
+            s = msg.substring(0, 8) + "...";
+        } else {
+            s = msg;
+        }
+        Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT);
+        View layout = View.inflate(getActivity(),
+                AppConfig.resourceId(getActivity(), "jmtoast", "layout"), null);
+        TextView user = (TextView) layout.findViewById(AppConfig.resourceId(getActivity(), "tvuser", "id"));
+        // 设置toast文本，把设置好的布局传进来
+        user.setText(s + "");
+        toast.setView(layout);
+        toast.setGravity(Gravity.TOP, 0, 50);
+        toast.show();
+    }
 
-	/**
-	 * 接口返回数据处理
-	 */
-	public void sendData(int num, Object data, Handler callback) {
-		Message msg = callback.obtainMessage();
-		msg.what = num;
-		msg.obj = data;
-		msg.sendToTarget();
-	}
-
-	/**
-	 * toast 提示信息
-	 * 
-	 * @param msg
-	 */
-	public void showUserMsg(String msg) {
-		String s = "";
-		if (msg.length() > 8) {
-			s = msg.substring(0, 8) + "...";
-		}else{
-			s=msg;
-		}
-		Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT);
-		View layout = View.inflate(getActivity(),
-				AppConfig.resourceId(getActivity(), "jmtoast", "layout"), null);
-		TextView user = (TextView) layout.findViewById(AppConfig.resourceId(getActivity(), "tvuser", "id"));
-		// 设置toast文本，把设置好的布局传进来
-		user.setText(s+"");
-		toast.setView(layout);
-		toast.setGravity(Gravity.TOP, 0, 50);
-		toast.show();
-	}
-
-	/**
-	 * 皮肤9切换提示信息
-	 *
-	 * @param msg
-	 */
-	public void showUserMsg_skin9(String msg) {
-		String s = "";
-		if (msg.length() > 8) {
-			s = msg.substring(0, 8) + "...";
-		}else{
-			s=msg;
-		}
-		Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG);
-		View layout = View.inflate(getActivity(),
-				AppConfig.resourceId(getActivity(), "jmautologin_9", "layout"), null);
-		TextView user = (TextView) layout.findViewById(AppConfig.resourceId(getActivity(), "tvusername", "id"));
-		TextView loginHomePage = (TextView) layout.findViewById(AppConfig.resourceId(getActivity(), "btbacklogin", "id"));
-		loginHomePage.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
+    /**
+     * 皮肤9切换提示信息
+     *
+     * @param msg
+     */
+    public void showUserMsg_skin9(String msg) {
+        String s = "";
+        if (msg.length() > 8) {
+            s = msg.substring(0, 8) + "...";
+        } else {
+            s = msg;
+        }
+        Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG);
+        View layout = View.inflate(getActivity(),
+                AppConfig.resourceId(getActivity(), "jmautologin_9", "layout"), null);
+        TextView user = (TextView) layout.findViewById(AppConfig.resourceId(getActivity(), "tvusername", "id"));
+        TextView loginHomePage = (TextView) layout.findViewById(AppConfig.resourceId(getActivity(), "btbacklogin", "id"));
+        loginHomePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 //				Intent intent = new Intent(getActivity(), JmSwitchLogin9Fragment.class);
 //				startActivity(intent);
-			}
-		});
-		// 设置toast文本，把设置好的布局传进来
-		user.setText(s+"");
-		toast.setView(layout);
-		toast.setGravity(Gravity.TOP, 0, 50);
-		toast.show();
-	}
+            }
+        });
+        // 设置toast文本，把设置好的布局传进来
+        user.setText(s + "");
+        toast.setView(layout);
+        toast.setGravity(Gravity.TOP, 0, 50);
+        toast.show();
+    }
 
-	
-	public void showMsg(String msg) {
-		DialogUtils.showTip(getActivity(), msg);
-	}
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		// 拦截触摸事件，防止泄露下去
-		view.setOnTouchListener(this);
-	}
+    public void showMsg(String msg) {
+        DialogUtils.showTip(getActivity(), msg);
+    }
 
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // 拦截触摸事件，防止泄露下去
+        view.setOnTouchListener(this);
+    }
 
-	public void turnToIntent(String url) {
-		if (TextUtils.isEmpty(url)) {
-			// Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
-			return;
-		}
-	
-		  Intent intent = new Intent();
-		  intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-		  Intent.FLAG_ACTIVITY_SINGLE_TOP); intent.putExtra("url", url);
-		  intent.setClass(getActivity(), JmUserinfoActivity.class);
-		 getActivity().startActivity(intent);
-		
-	}
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        // TODO Auto-generated method stub
+        return true;
+    }
 
-	public void turnToNotice(final String url) {
-		if (TextUtils.isEmpty(url)) {
-			// Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
-			return;
-		}
+    public void turnToIntent(String url) {
+        if (TextUtils.isEmpty(url)) {
+            // Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        FloatUserInfoActivity floatUserInfoActivity = new FloatUserInfoActivity((Activity) JiMiSDK.mContext, new FloatUserInfoActivity.CloseFloatListener() {
+            @Override
+            public void closeFloat() {
+            }
+        });
+        floatUserInfoActivity.notice = true;
+        floatUserInfoActivity.setViews(url);
+        floatUserInfoActivity.show();
 
-		JiMiSDK.handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				Intent intent = new Intent();
-				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-						Intent.FLAG_ACTIVITY_SINGLE_TOP);
-				intent.putExtra("url", url);
-				intent.putExtra("notice", true);
-				intent.setClass(JiMiSDK.mContext, JmUserinfoActivity.class);
-                JiMiSDK.mContext.startActivity(intent);
-			}
-		}, 1000);
-	}
+    }
 
-	public void toAutologin(Registermsg registermsg) {
-		Intent intent = new Intent(getActivity(), JmAutoLoginActivity.class);
-		startActivity(intent);
-	}
+    public void turnToNotice(final String url) {
+        if (TextUtils.isEmpty(url)) {
+            // Toast.makeText(mContext, "此功能暂未开通", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        JiMiSDK.handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FloatUserInfoActivity floatUserInfoActivity = new FloatUserInfoActivity((Activity) JiMiSDK.mContext, new FloatUserInfoActivity.CloseFloatListener() {
+                    @Override
+                    public void closeFloat() {
+                    }
+                });
+                floatUserInfoActivity.notice = true;
+                floatUserInfoActivity.setViews(url);
+                floatUserInfoActivity.show();
+            }
+        }, 1000);
+    }
+
+    public void toAutologin(Registermsg registermsg) {
+        Intent intent = new Intent(getActivity(), JmAutoLoginActivity.class);
+        startActivity(intent);
+    }
 
 }
